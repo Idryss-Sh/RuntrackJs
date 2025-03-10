384303a1e95c1329091e719f24eb5dd1086ac320
Idryss FEHAM <idryss2609@gmail.com>
2025-03-10 16:32:04 +0100

    Ecrire Hello Javascript dans la console web

--- Jour01/Job02/script.js
+++ Jour01/Job02/script.js
@@ -1,7 +1,1 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    const button = document.getElementById('button-popup');
-
-    button.addEventListener('click', () => {
-        alert('Hello Javascript!');
-    });
-});
+alert("Hello Javascript");
