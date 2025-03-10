fc6c249df756c2af0cd5e4684624fda80fc71a3e
Idryss FEHAM <idryss2609@gmail.com>
2025-03-10 10:55:14 +0100

    Faire un popup avec un fichier js

---
+++ Jour01/Job02/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const button = document.getElementById('button-popup');
+
+    button.addEventListener('click', () => {
+        alert('Hello Javascript!');
+    });
+});
